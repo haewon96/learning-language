ae412db3f2bfb263196bc5bcd61547e44bfc6ca0
yunji <zion830@naver.com>
2018-09-27 15:24:13 +0900

    프로젝트 생성

---
+++ app/src/main/java/org/sungshin/lnk/learningnorthkorean/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package org.sungshin.lnk.learningnorthkorean.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import org.sungshin.lnk.learningnorthkorean.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
